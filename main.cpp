b7baafe96d2cff1c3a4fb13048db5fa1076b776b
DESKTOP-Q4SH08D\racar <racareanu2211@gmail.com>
2024-10-29 17:59:04 +0200

    -solved first task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,25 +10,53 @@ int main()
     ifstream inFile("input.txt");
 
     int no_problems, no_doctors;
-    string name, speciality;
+    struct problem
+    {
+        string name, speciality;
+    };
+    
     
     inFile >> no_problems;
+    vector<struct problem> prb_list;
 
     for (int i = 0; i < no_problems; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        struct problem prb;
+        inFile >> prb.name;
+        inFile >> prb.speciality;
+        cout << prb.name << ' ' << prb.speciality << '\n';
+        prb_list.push_back(prb);
     }
 
     inFile >> no_doctors;
 
+    struct doctor {
+        string name, speciality;
+    };
+
+    vector<struct doctor> doc_list;
+
     for (int i = 0; i < no_doctors; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        struct doctor doc;
+        inFile >> doc.name;
+        inFile >> doc.speciality;
+        cout << doc.name << ' ' << doc.speciality << '\n';
+        doc_list.push_back(doc);
     }
 
+    for (auto& problem : prb_list)
+    {
+        bool isFound = 0;
+        for (auto& doctor : doc_list)
+        {
+            if (problem.speciality == doctor.speciality)
+            {
+                isFound = 1;
+                break;
+            }
+        }
+        isFound ? cout << problem.name << " Acceptat" << endl : cout << problem.name << " Respins" << endl;
+    }
     return 0;
 }
